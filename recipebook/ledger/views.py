7b956c434b32fd0c2eeea870c30987b7473a93eb
Nathan Sapitula <teetan888@gmail.com>
2025-02-24 20:06:17 +0800

    Finished Individual Recipe Entries

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -68,4 +68,67 @@ def recipeList(request):
     return render(request, "recipeList.html", ctx)
 
 def recipe(request, num=1):
-    pass
+    if num == 1:
+        ctx = {
+            "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
+                }
+            ],
+            "link": "/recipe/1"
+        }
+    elif num == 2:
+        ctx = {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipe/2"
+        }
+
+    return render(request, "recipeEntry.html", ctx)
