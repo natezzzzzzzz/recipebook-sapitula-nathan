7d53c546ac910406d0f120c729ba36b4017a9ac6
Nathan Sapitula <teetan888@gmail.com>
2025-02-24 19:58:50 +0800

    Completed Recipe List Page, Minor Bug Fix
    Bug: Had an extra / in the url path in urls.py of the ledger app

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = {
-    path("/recipes/list", recipeList, name="recipeList"),
-    path("/recipe/<int:num>", recipe, name="recipe"),
+    path("recipes/list", recipeList, name="recipeList"),
+    path("recipe/<int:num>", recipe, name="recipe"),
 }
