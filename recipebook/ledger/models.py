31e1278cf744419245df141b7227558edc75edd7
Nathan Sapitula <teetan888@gmail.com>
2025-03-06 20:40:50 +0800

    Created String Representations and Absolute URLs for Ingredient and Recipe

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return '{}'.format(self.name)
+
+    def get_absolute_url(self):
+        return reverse('ingredient', args=[str(self.id)])
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+    def get_absolute_url(self):
+        return reverse('recipe', args=[str(self.id)])
 
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
