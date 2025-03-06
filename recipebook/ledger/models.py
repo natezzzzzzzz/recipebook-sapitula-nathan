0c18c9c8ba0b1491d7c246432768804aa9ad9900
Nathan Sapitula <teetan888@gmail.com>
2025-03-06 20:36:39 +0800

    Created 3 models: Ingredient, Recipe, RecipeIngredient

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=5, decimal_places=0)
